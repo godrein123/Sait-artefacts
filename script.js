486c565b406f45a020efb52e1706d18cfa638d8c
Yurii Chebotarev <yuramba123@mail.ru>
2024-07-26 17:37:40 +1000

    (add functional button Burger)

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-if (window.jQuery) {
-   //jQuery подключен к странице
+const burger = document.querySelector('.burger');
+const nuv = document.querySelector('.nuv');
+
+function toggleNuv() {
+	if (nuv.style.display === "none" || nuv.style.display === "") {
+		nuv.style.display = "block";
+		setTimeout(() => {
+		nuv.style.opacity = 1;	
+		}, 150);
+		} else {
+			nuv.style.opacity = 0;
+		setTimeout(() => {
+			nuv.style.display = "none";
+		}, 150);
+		}
 }
 
+burger.addEventListener("click", toggleNuv);
 
-$(".burger").click(function() {
-  $(".nuv").slideToggle(400);
-});
+function nuvResize() {
+	if (window.innerWidth > 900) {
+		nuv.style.display = 'block';
+		burger.style.display = 'none';
+	} else {
+		nuv.style.display = 'none';
+		burger.style.display = 'block';
+	}
+}
 
-$(window).resize(function() {
-  if ($(window).width() > 900) {
-    $(".nuv").show();
-    $(".burger").hide();
-  } else {
-    $(".nuv").hide();
-    $(".burger").show();
-  }
-});
+window.addEventListener("resize",nuvResize);
+
+nuvResize();
